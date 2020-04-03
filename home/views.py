f6200f47dc3f5e6adcc044f7a307da3f22e5b13f
ireem18 <irem_erencan@hotmail.com>
2020-04-04 00:14:42 +0300

    ContactFormMessage modeli oluşturuldu

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
-from home.models import Setting
+from home.models import Setting, ContactForm, ContactFormMessage
 
 
 def index(request):
@@ -20,6 +21,20 @@ def referanslar(request):
         return render(request, 'referanslarimiz.html',context)
 
 def iletisim(request):
+
+        if request.method == 'POST': # Form post edildiyse
+                form = ContactForm(request.POST)
+                if form.is_valid():
+                        data = ContactFormMessage() # model ile bağlantı kur
+                        data.name = form.cleaned_data['name'] # formdan bilgiyi al
+                        data.email = form.cleaned_data['email']
+                        data.subject = form.cleaned_data['subject']
+                        data.message = form.cleaned_data['message']
+                        data.save() # veritabanına kayıt et
+                        messages.success(request, "Mesajınız basarı ile gönderildi")
+                        return HttpResponseRedirect('/iletisim')
+
         setting = Setting.objects.get(pk=2)
-        context = {'setting':setting, 'page':'iletisim'}
+        form = ContactForm()
+        context = {'setting':setting, 'form': form}
         return render(request, 'iletisim.html',context)
